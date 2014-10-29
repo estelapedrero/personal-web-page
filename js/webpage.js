921d5f5f48696b4cb2927ed4a5eee43270749fb3
Estela <estelapedrero@gmail.com>
2014-10-29 09:12:31 +0100

    links correctos

--- js/webpage.js
+++ js/webpage.js
@@ -1,16 +1,18 @@
-var imagen=document.getElementsByClassName('img_info')
+var caja=document.getElementsByClassName('magia');
 
-var texto=document.getElementsByClassName('imgs_txt')
+var texto=document.getElementsByClassName('imgs_txt');
 
 
 function quitar(e){
-	e.target.className='img_transp'
+	e.target.className='img_transp';
+	e.target.firstElementChild.className=('imgs_circulo');
 }
 function poner(e){
-	e.target.className='img_info'
+	e.target.className='magia';
+	e.target.firstElementChild.className=('imgs_txt');
 }
 
-for(var i = 0; i < imagen.length; i++){
-    imagen[i].addEventListener('mouseenter', quitar);
-    imagen[i].addEventListener('mouseleave', poner);
+for(var i = 0; i < caja.length; i++){
+    caja[i].addEventListener('mouseenter', quitar);
+    caja[i].addEventListener('mouseleave', poner);
 }
